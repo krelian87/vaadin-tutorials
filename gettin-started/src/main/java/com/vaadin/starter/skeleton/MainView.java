58aed0ec7693c65690be7ccea4d3022e98c5748b
Marco Sbaffoni <m.sbaffoni@eustema.it>
2019-02-03 11:39:46 +0100

    Initial commit + part 3: Creating Live Filtering for Entities

---
+++ gettin-started/src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -0,0 +1,54 @@
+package com.vaadin.starter.skeleton;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
+public class MainView extends VerticalLayout {
+
+    private static final long serialVersionUID = -6156495495853207451L;
+    private CustomerService service = CustomerService.getInstance();
+    private Grid<Customer> grid = new Grid<>();
+    private TextField filterText = new TextField();
+
+    public MainView() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setValueChangeMode(ValueChangeMode.EAGER);
+        filterText.addValueChangeListener(e -> updateList());
+
+        Button clearFilterTextBtn = new Button(
+            new Icon(VaadinIcon.CLOSE_CIRCLE));
+        clearFilterTextBtn.addClickListener(e -> filterText.clear());
+
+        HorizontalLayout filtering = new HorizontalLayout(filterText,
+        clearFilterTextBtn);
+        add(filtering, grid);
+
+        grid.setSizeFull();
+
+        grid.addColumn(Customer::getFirstName).setHeader("First name");
+        grid.addColumn(Customer::getLastName).setHeader("Last name");
+        grid.addColumn(Customer::getStatus).setHeader("Status");
+
+        add(grid);
+        setHeight("100vh");
+
+        updateList();
+    }
+
+    private void updateList() {
+        grid.setItems(service.findAll(filterText.getValue()));
+    }
+}
